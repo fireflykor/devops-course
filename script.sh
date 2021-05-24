661c080f1a6f13e60c2a1a5d2c7638632071e90e
firefly <vadik777k@gmail.com>
2021-05-24 22:25:39 +0500

    Добавил скприт для вывода погоды в городе

---
+++ script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+param=$(curl -s wttr.in/$1?format=j1 | jq '."current_condition"[0]')
+temp=$(echo "$param" | jq .temp_C | tr -d \")
+humidity=$(echo "$param" | jq .humidity | tr -d \")
+echo "City: $1"
+echo "Temperature: $temp C"
+echo "Humidity: $humidity %"
