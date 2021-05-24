36536f0273e7ec6b37666d71fe0891f4287be52f
firefly <vadik777k@gmail.com>
2021-05-24 22:50:05 +0500

    Removed the city output

--- weather_script.sh
+++ weather_script.sh
@@ -3,6 +3,5 @@
 param=$(curl -s wttr.in/$1?format=j1 | jq '."current_condition"[0]')
 temp=$(echo "$param" | jq .temp_C | tr -d \")
 humidity=$(echo "$param" | jq .humidity | tr -d \")
-echo "City: $1"
 echo "Temperature: $temp C"
 echo "Humidity: $humidity %"
